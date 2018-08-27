e3c5423dac97aa4cb290f19f77553856aa153959
agata <MicaOlaAdams@sharklasers.com>
2018-08-27 11:49:24 +0200

    erstes Skelett

--- 2018-08-24 11 bagels.py
+++ 2018-08-24 11 bagels.py
@@ -6,6 +6,37 @@ import
 ########## Constants ##########
 
 ########## Functions ##########
+def randomNumber():
+  '''
+  - make a random number of three digits
+  - no repeating digits
+  - return number
+  '''
+
+def printIntro():
+  '''
+  - print Intro Text
+  '''
+
+def playerGuess():
+  '''
+  - ask player to guess
+  - validate player input
+  - only digits 0-9
+  - no repeating
+  '''
+
+def checkWin(randomNumber, guess):
+  '''
+  - returns False, if guess is the same as the randomNumber
+  '''
+
+def checkClues(randomNumber, guess):
+  '''
+  - check guess for clues
+  - print clues
+  '''
+
 def gameChoice():
   '''
   - asks the player, whether he wants to play again and validates the input
@@ -36,6 +67,18 @@ while gameState == True:
 ########## Round Loop #########
   while gameRound == True:
     # remains true for every round of the game
+    for x in xrange(1,10):
+      randomNumber = randomNumber()
+      printIntro()
+      guess = playerGuess()
+      gameRound = checkWin(randomNumber, guess)
+
+      if gameRound:
+        checkClues(randomNumber, guess)
+      else:
+        break
+
+    gameRound = False
 
   # ?does the player want to play again?
   gameChoice()
