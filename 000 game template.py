327239719ce34e2a9e5dd1fc4ea17d6d371b29be
agata <MicaOlaAdams@sharklasers.com>
2018-09-10 09:45:41 +0200

    update game template

--- 000 game template.py
+++ 000 game template.py
@@ -8,11 +8,9 @@ import
 ########## Functions ##########
 def gameChoice():
   '''
-  - asks the player, whether he wants to play again and validates the input
+  - asks player, whether he wants to play again and validates the input
   - accepts only "y" or "n"
-  - sets the gameState accordingly
   '''
-
   gameChoice = ''
 
   while gameChoice != 'y' and gameChoice != 'n':
@@ -29,13 +27,13 @@ gameState = True
 # remains true for as long, as the player wants to keep playing
 
 while gameState == True:
-    ###### Globals ########
-    gameRound = True
-    # remains true for as long as the game is neither lost or won
+  ###### Globals ########
+  gameRound = 1
+  winState = False
+  printIntro()
 
-########## Round Loop #########
-  while gameRound == True:
-    # remains true for every round of the game
+  ########## Round Loop #########
+  while gameRound != 11 and winState == False:
 
   # ?does the player want to play again?
   gameState = gameChoice()
