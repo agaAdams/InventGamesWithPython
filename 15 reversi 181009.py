f1678fffd1f3dd4bd44a8f6019f0343787651c86
agata <MicaOlaAdams@sharklasers.com>
2018-10-12 12:49:13 +0200

    move
    - make player move

--- 15 reversi 181009.py
+++ 15 reversi 181009.py
@@ -9,17 +9,29 @@ HEIGHT = 8
 ########## Functions ##########
 def printIntro():
   print("Welcome to Reversi!")
-  role = ''
+  playerRole = ''
+  aiRole = ''
 
-  while role != "x" and role != "o":
+  while playerRole != "x" and playerRole != "o":
     print("Do you want to be X or O?")
-    role = input()
+    playerRole = input()
 
-  return role
+  playerRole = playerRole.upper()
+
+  if playerRole == 'X':
+    aiRole = 'O'
+  else:
+    aiRole = 'X'
+  return playerRole, aiRole
 
 def randomizeTurn():
-  roles = [player,ai]
-  return random.choice(roles)
+  roles = ['player','ai']
+  playerRole = random.choice(roles)
+  if playerRole == 'player':
+    print("The player will go first.")
+  else:
+    print("The computer will go first.")
+  return playerRole
 
 def firstLine():
   first = '   '
@@ -48,6 +60,10 @@ def makeNewBoard():
     board.append([])
     for y in range(HEIGHT):
       board[x].append(' ')
+  board[3][3] = 'X'
+  board[4][3] = 'O'
+  board[3][4] = 'O'
+  board[4][4] = 'X'
 
 def printBoard():
   print(firstLine())
@@ -61,13 +77,56 @@ def printBoard():
     print('   ' + horizontalLine())
   print(borderLine())
 
-def setMarker(role):
-  print("Enter move:")
-  playerInput = input()
-  y = int(playerInput[0]) - 1
-  x = int(playerInput[1]) - 1
+def calculatePoints():
+  xPoints = 0
+  oPoints = 0
+
+  for x in range(WIDTH):
+    for y in board[x]:
+      if y == 'X':
+        xPoints += 1
+      elif y == 'O':
+        oPoints += 1
+
+  if playerRole == 'X':
+    print("You have %s points. The computer has %s points." % (xPoints, oPoints))
+  else:
+    print("You have %s points. The computer has %s points." % (oPoints, xPoints))
+
+def playerMove():
+  playerInput = ''
+  validInput = False
+
+  while validInput == False:
+    print("Enter your move, or type quit to end the game, or hints to show hints.")
+    playerInput = input()
+    if playerInput == 'quit':
+      sys.exit()
+    elif playerInput.isdigit() and len(playerInput) == 2:
+      if int(playerInput[0]) > 0 and int(playerInput[0]) <= WIDTH and int(playerInput[1]) > 0 and int(playerInput[1]) <= HEIGHT:
+        y = int(playerInput[0]) - 1
+        x = int(playerInput[1]) - 1
+        if checkFields(x, y) == False:
+          validInput = True
+          setMarker(playerRole, y, x)
+        else:
+          print("This field is already taken.")
+
+def setMarker(role, y, x):
   board[x][y] = role
 
+def checkFields(x, y):
+  for line in range(WIDTH):
+    if line == x and y in board[line]:
+      return True
+  return False
+
+def aiMove():
+  input("Press Enter to see the computer's move.")
+  setMarker(aiRole, 2, 2)
+
+def checkPossibleMoves():
+  pass
 def gameChoice():
   '''
   - asks player, whether he wants to play again and validates the input
@@ -86,19 +145,24 @@ gameState = True
 while gameState == True:
   ###### Globals ########
   gameRound = 1
-  winState = False
+  endState = False
   board = []
   makeNewBoard()
-  playerRole = printIntro()
+  playerRole, aiRole = printIntro()
   turnState = randomizeTurn()
 
   ########## Round Loop #########
-  while gameRound != (WIDTH * HEIGHT) and winState == False:
+  while gameRound != (WIDTH * HEIGHT) and endState == False:
     printBoard()
+    calculatePoints()
     if turnState == 'player':
-      pass
-    setMarker(playerRole)
+      playerMove()
+      turnState = 'ai'
+    elif turnState == 'ai':
+      aiMove()
+      turnState = 'player'
     gameRound += 1
+    endState = checkPossibleMoves()
 
   # ?does the player want to play again?
   gameState = gameChoice()
