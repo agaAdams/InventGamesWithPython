c1ebfa7153748f8df7e093f3542175d552227737
agata <MicaOlaAdams@sharklasers.com>
2018-11-06 11:30:32 +0100

    Ordner aufräumen

--- 000 game template.py
+++ 000 game template.py
@@ -11,29 +11,26 @@ def gameChoice():
   - asks player, whether he wants to play again and validates the input
   - accepts only "y" or "n"
   '''
-  gameChoice = ''
-
-  while gameChoice != 'y' and gameChoice != 'n':
-      print("Do you want to play again? (y/n)")
-      gameChoice = input()
-
-  if gameChoice == 'y':
-      return True
-
-  return False
+  print("Do you want to play again? (y/n)")
+  if input() == 'y':
+    return True
+  else:
+    sys.exit()
 
 ########## Game Loop ##########
 gameState = True
 # remains true for as long, as the player wants to keep playing
 
 while gameState == True:
+  '''basic game loop'''
   ###### Globals ########
   gameRound = 1
-  winState = False
+  endState = False
   printIntro()
 
   ########## Round Loop #########
-  while gameRound != 11 and winState == False:
+  while endState == False:
+    '''basic round loop'''
 
   # ?does the player want to play again?
   gameState = gameChoice()
